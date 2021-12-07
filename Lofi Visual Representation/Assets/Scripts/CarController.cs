f31a0df3b15ec3e68a78a23d2b89d0004c271570
Dylan <dylanoconnor32@gmail.com>
2021-12-07 21:01:45 +0000

    Adding Gun + Crosshair

--- Lofi Visual Representation/Assets/Scripts/CarController.cs
+++ Lofi Visual Representation/Assets/Scripts/CarController.cs
@@ -16,24 +16,30 @@ public class CarController : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        rb.useGravity = false;
         jump = new Vector3(0.0f, 3.0f, 0.0f);
     }
 
     void OnCollisionStay(){
         isGrounded = true;
+        
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {   
-        if(Input.GetKeyDown(KeyCode.Space) && isGrounded){
+        /*if(Input.GetKeyDown(KeyCode.Space) && isGrounded){
             rb.AddForce(jump * jumpForce, ForceMode.Impulse);
+            rb.useGravity = true;
             isGrounded = false;
         }
-
+        
+        if(Input.GetKeyDown(KeyCode.UpArrow)){
+            rb.useGravity = false;
+        }*/
+        
         float horMovement = Input.GetAxis("Horizontal") * speed;
         float verMovement = Input.GetAxis("Vertical") * speed;
-
         transform.Translate(new Vector3(horMovement, 0, verMovement) * Time.deltaTime);
     }
 
---
+++ Lofi Visual Representation/Assets/Scripts/GunController.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunController : MonoBehaviour
+{
+    public float damage = 10f;
+    public float range = 100f;
+
+    public CarController fpsGun;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)){
+            Shoot();
+        }
+    }
+
+    void Shoot(){
+        RaycastHit hit;
+        if(Physics.Raycast(fpsGun.transform.position, fpsGun.transform.forward, out hit, range)){
+            Debug.Log(hit.transform.name);
+
+            Target target = hit.transform.GetComponent<Target>();
+        }
+    }
+}
