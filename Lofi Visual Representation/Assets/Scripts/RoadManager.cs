e87b95a6b2723272ad67d9af6549d31cee61a5b9
Dylan <dylanoconnor32@gmail.com>
2021-12-08 21:08:18 +0000

    Spawning new inifinite road

--- Lofi Visual Representation/Assets/Scripts/RoadManager.cs
+++ Lofi Visual Representation/Assets/Scripts/RoadManager.cs
@@ -9,10 +9,15 @@ public class RoadManager : MonoBehaviour
     public float roadLength = 30;
     public int numOfRoads = 3;
     // Start is called before the first frame update
+    public Transform playerTransform;
     void Start()
     {
         for(int i=0; i<numOfRoads; i++){
-            SpawnRoad(Random.Range(0, roadPrefabs.Length));
+            if(i==0){
+                SpawnRoad(0);
+            } else {
+                SpawnRoad(Random.Range(0, roadPrefabs.Length));
+            }
         }
   
     }
@@ -20,7 +25,9 @@ public class RoadManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(playerTransform.position.z>zSpawn-(numOfRoads*roadLength)){
+            SpawnRoad(Random.Range(0, roadPrefabs.Length));
+        }
     }
 
     public void SpawnRoad(int roadIndex){
