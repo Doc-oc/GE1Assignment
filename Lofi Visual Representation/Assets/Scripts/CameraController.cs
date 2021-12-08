4ac30be5b50acc39965255f35260f017503e912b
Dylan <dylanoconnor32@gmail.com>
2021-12-08 15:24:56 +0000

    Fixing project upon startup

--- Lofi Visual Representation/Assets/Scripts/CameraController.cs
+++ Lofi Visual Representation/Assets/Scripts/CameraController.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
 
-    private Transform car;
+    private Transform spaceship;
 
     private float zOffset = -9f;
     private float yOffset = 2f;
@@ -13,13 +13,13 @@ public class CameraController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     { 
-        car = GameObject.Find("Spaceship").transform;
+        spaceship = GameObject.Find("Spaceship").transform;
         
     }
 
     // Update is called once per frame
     void LateUpdate()
     {
-        transform.position = new Vector3(car.position.x, car.position.y + yOffset, car.position.z + zOffset);
+        transform.position = new Vector3(spaceship.position.x, spaceship.position.y + yOffset, spaceship.position.z + zOffset);
     }
 }
