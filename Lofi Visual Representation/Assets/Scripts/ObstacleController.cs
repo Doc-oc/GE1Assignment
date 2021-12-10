82f5df92728a3cdfadada690cca2829e52d6e55d
Dylan <dylanoconnor32@gmail.com>
2021-12-10 16:17:01 +0000

    Adding AI Ships

--- Lofi Visual Representation/Assets/Scripts/ObstacleController.cs
+++ Lofi Visual Representation/Assets/Scripts/ObstacleController.cs
@@ -7,23 +7,8 @@ public class ObstacleController : MonoBehaviour
 private float speed = 3f;
 private bool going_right = false;
 
-void Update() 
-{
-    if(going_right) 
+    void Update() 
     {
         transform.Translate(Vector3.forward * Time.deltaTime * speed); // Move right
-        if(transform.position.x > 7) // Too far right
-        { 
-            going_right = false; // Switch direction
-        }
     }
-    else 
-    {
-        transform.Translate(-Vector3.forward * Time.deltaTime * speed); // Move left
-        if(transform.position.x < -5) // Too far left
-        { 
-            going_right = true; // Switch direction
-        }
-    }
-}
 }
