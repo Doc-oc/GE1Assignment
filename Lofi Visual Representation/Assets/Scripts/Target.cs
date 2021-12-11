433df2194d9fec207bbc64758b78c94b7ce9c837
Dylan <dylanoconnor32@gmail.com>
2021-12-11 19:40:03 +0000

    Adding new Target script

--- Lofi Visual Representation/Assets/Scripts/Target.cs
+++ Lofi Visual Representation/Assets/Scripts/Target.cs
@@ -4,36 +4,17 @@ using UnityEngine;
 
 public class Target : MonoBehaviour
 {
-    public GameObject[] targetPrefab;
-    private List<GameObject> activeTarget = new List<GameObject>();
     public float health = 50f;
-    public float zSpawn = 0;
-    public int numOfTargs= 3;
-    public Transform playerTransform;
+    public GameObject[] targetPrefabs;
+    private List<GameObject> activeTarget = new List<GameObject>();
 
-    void Start()
-    {
-        for(int i=0; i<numOfTargs; i++){
-            SpawnTarget(Random.Range(0, targetPrefab.Length));
-            Debug.Log(targetPrefab.Length);
-        }
-  
-    }
-    void Update()
-    {
-        if(playerTransform.position.z > gameObject.transform.position.z || activeTarget.Count < 1){
-            Debug.Log("IN");
-            SpawnTarget(Random.Range(0, targetPrefab.Length));
-            Die();
-        }
+    void Start(){
+        Spawn(Random.Range(0, targetPrefabs.Length));
     }
 
-    public void SpawnTarget(int tarIndex){
-        GameObject tObj = Instantiate(targetPrefab[tarIndex], transform.forward * zSpawn, transform.rotation);
-        //activeTarget.Add(tObj);
-        //zSpawn += 20;
+    void Update(){
+        //Spawn(Random.Range(0, targetPrefabs.Length));
     }
-
     public void TakeDamage(float amount){
         health -= amount;
         if (health <= 0f){
@@ -43,9 +24,10 @@ public class Target : MonoBehaviour
 
     void Die(){
         Destroy(gameObject);
-        //activeTarget.RemoveAt(0);
     }
 
-    
-
+    public void Spawn(int targetIndex){
+        GameObject gObj = Instantiate(targetPrefabs[targetIndex], transform.position , transform.rotation);
+        activeTarget.Add(gObj);
+    }
 }
