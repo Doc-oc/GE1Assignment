cc84b367f752582de5d328040edb45bc759ded49
Dylan <dylanoconnor32@gmail.com>
2021-12-14 13:37:02 +0000

    Adding Background music

--- Lofi Visual Representation/Assets/Scripts/Score.cs
+++ Lofi Visual Representation/Assets/Scripts/Score.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 
 public class Score : MonoBehaviour
 {
+    public Target target;
     public Transform player;
     public Text score;
     private ObstacleController obstacle;
@@ -14,15 +15,20 @@ public class Score : MonoBehaviour
     // Update is called once per frame
     void Start(){
         StartCoroutine(ScoreUpdate());
+        target = FindObjectOfType<Target>();
     }
 
     public void TargetScore(){
         score.text = (scoreText += 30).ToString();
+        
     }
     IEnumerator ScoreUpdate()
     {
         yield return new WaitForSeconds(2);
         score.text = (scoreText += 10).ToString();
+        if(scoreText % 100 == 0){
+            target.Spawn();
+        }
         StartCoroutine(ScoreUpdate());
     }
 }
